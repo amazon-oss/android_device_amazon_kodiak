8a4e5ddf762a4b0a4527a9dca1bd72dba96de2de
R0rt1z2 <me@r0rt1z2.com>
2025-01-06 19:50:13 +0100

    kodiak: Set platform flags
    Change-Id: I4ead8464bebf1f4289d976adb37d659b63d2f588

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,13 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+TARGET_USES_QCOM_BSP := true
+
 # Inherit the prebuilt kernel files
 include device/amazon/kodiak-kernel/BoardConfigKernel.mk
 
