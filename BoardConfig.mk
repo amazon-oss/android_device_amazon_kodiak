767ab80690c1e229d26d7ca03c9555edf2a1c4b4
R0rt1z2 <me@r0rt1z2.com>
2025-01-06 19:45:27 +0100

    kodiak: Set Architecture flags
    Change-Id: I05871677d47c0df72a0baf0552f7ad56ff8789b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,12 @@
 
 DEVICE_PATH := device/amazon/kodiak
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+
 # Inherit the proprietary files
 -include vendor/amazon/kodiak/BoardConfigVendor.mk
